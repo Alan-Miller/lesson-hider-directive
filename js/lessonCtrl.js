895428086cc8e32ba7584325578e3e5b4449bda6
Alan-Miller <alan.ashman@gmail.com>
2016-09-07 03:24:15 -0600

    try2 changes

---
+++ js/lessonCtrl.js
@@ -0,0 +1,7 @@
+angular.module('lessonApp').controller('lessonCtrl', function() {
+
+
+  $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+
+
+});
---
+++ js/lessonService.js
@@ -0,0 +1,8 @@
+angular.module('lessonApp').service('lessonService', function() {
+
+
+
+
+
+
+});
